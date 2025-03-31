a4bcf6ccf08a3001665b8adc412ee9aea19ffb99
edjziii <edjozap@gmail.com>
2025-04-01 00:31:45 +0800

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using CS2AZapicoEdwardJoseph_MVCProject.BusLogic.Model;
+using CS2AZapicoEdwardJoseph_MVCProject.BusLogic.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS2AZapicoEdwardJoseph_MVCProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentService _studentService = new StudentService();
+
+        public IActionResult Index()
+        {
+            var students = _studentService.GetAll();
+            return View(students);
+        }
+        public IActionResult AddNewStudent()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditStudent(int ID)
+        {
+            var student = _studentService.GetById(ID);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(tblStudent student)
+        {
+            try
+            {
+                bool result = _studentService.Add(student);
+                return Json(new { success = result, message = result ? "Student added successfully!" : "Failed to add student :("});
+            } catch (Exception ex)
+            {
+                return Json(new {success = false, message = ex.Message});
+            }
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿const AddStudent = async (student) => {
+    try {
+        const result = await AjaxPOST('/Student/AddStudent', student);
+        if (result.success) {
+            alert('Student added succesfully!');
+            window.location.href = '/Student/Index';
+        } else {
+            alert('Failed to add student: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Error adding student: ', error);
+        alert('An error occured while adding the student.');
+    }
+}
+
+$(document).ready(function () {
+    $('#addStudentForm').on('submit', function (e) {
+        e.preventDefault();
+        const student = {
+            FirstName: $('#FirstName').val(),
+            LastName: $('#LastName').val(),
+            Age: $('#Age').val(),
+            Course: $('#Course').val(),
+        };
+        AddStudent(student);
+    });
+});
